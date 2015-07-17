a9e69f8b1152dd835e9b4b398ebe164312447ec0
Conor Reid <conor.will.reid@gmail.com>
2015-07-17 11:37:15 -0400

    added tpq

--- scraper.py
+++ scraper.py
@@ -72,127 +72,294 @@ for i in range(n):
 	#this tests for the length of the line
 	y = len(test_text_split)
 	#this checks if the line matches the hoard name case
-	if type_match(test_text):
-		type = test_text_split[1:(y-1)]
-	elif leader_match1(test_text) or leader_match2(test_text):
-		leader = test_text_split[0:(y-1)]
-	elif dynasty_match(test_text):
-		dynasty = test_text_split[1:(y-1)]
-		#also need to reset leader field with each new dynasty, as that dynasty may not have a leader type
-		leader = ""
-	elif hoard_name_match(test_text):
-		hoard_name = test_text_split[1:y]
-		weight = ""
-	elif mint_line_match1(test_text) or mint_line_match2(test_text):
-		mint = ""
-		mint_line = test_text
-		y = len(test_text_split)
-		for w in range(y):
-			if mint_match1(test_text_split[w]) or mint_match2(test_text_split[w]):
-				if mint == "":
-					mint = test_text_split[w]
-				else:
-					mint = mint + " " + test_text_split[w]
-			if date_match1(test_text_split[w]) or date_match2(test_text_split[w]) or date_match3(test_text_split[w]):
-				date = test_text_split[w]
-				if w+2 < y:
-					if weight_match(test_text_split[w+1]) and just_frag(test_text_split[w+2]):
-						weight = test_text_split[w+1]
-						comment = "fragment"
-						print(region, hoard_name, type, dynasty, leader, mint, date, weight, comment)
-					elif weight_match(test_text_split[w+1]):
-						weight = test_text_split[w+1]
-						print(region, hoard_name, type, dynasty, leader, mint, date, weight)
-					if just_multiple(test_text_split[w+1]):
-						j = int(filter(unicode.isdigit, test_text_split[w+1]))
-						z = 0
-						h = 0
-						while z < j:
-							if w+3+h < y:
-								if weight_match(test_text_split[w+2+h]) and just_frag(test_text_split[w+3+h]):
-									weight = test_text_split[w+2+h]
-									comment = "fragment"
-									print(region, hoard_name, type, dynasty, leader, mint, date, weight, comment)
-									z = z + 1
-									h = h + 1
-								elif weight_match(test_text_split[w+2+h]):
-									weight = test_text_split[w+2+h]
-									print(region, hoard_name, type, dynasty, leader, mint, date, weight)
-									z = z + 1
-									h = h + 1
+	if i-1 >= 0: 
+		if type_match(test_text):
+			type = test_text_split[1:(y-1)]
+		elif hoard_name_match(doc.paragraphs[i-1].text):
+			tpqlen = len(doc.paragraphs[i].runs)
+			for x3 in range(tpqlen):
+				if doc.paragraphs[i].runs[x3].bold == True:
+					tpq = doc.paragraphs[i].runs[x3].text
+		elif leader_match1(test_text) or leader_match2(test_text):
+			leader = test_text_split[0:(y-1)]
+		elif dynasty_match(test_text):
+			dynasty = test_text_split[1:(y-1)]
+			#also need to reset leader field with each new dynasty, as that dynasty may not have a leader type
+			leader = ""
+		elif hoard_name_match(test_text):
+			hoard_name = test_text_split[1:y]
+			weight = ""
+			tpq = ""
+		elif mint_line_match1(test_text) or mint_line_match2(test_text):
+			mint = ""
+			mint_line = test_text
+			y = len(test_text_split)
+			for w in range(y):
+				if mint_match1(test_text_split[w]) or mint_match2(test_text_split[w]):
+					if mint == "":
+						mint = test_text_split[w]
+					else:
+						mint = mint + " " + test_text_split[w]
+				if date_match1(test_text_split[w]) or date_match2(test_text_split[w]) or date_match3(test_text_split[w]):
+					date = test_text_split[w]
+					if w+2 < y:
+						if weight_match(test_text_split[w+1]) and just_frag(test_text_split[w+2]):
+							weight = test_text_split[w+1]
+							comment = "fragment"
+							print(region, hoard_name, type, dynasty, leader, mint, date, weight, comment)
+						elif weight_match(test_text_split[w+1]):
+							weight = test_text_split[w+1]
+							print(region, hoard_name, type, dynasty, leader, mint, date, weight)
+						if just_multiple(test_text_split[w+1]):
+							j = int(filter(unicode.isdigit, test_text_split[w+1]))
+							z = 0
+							h = 0
+							while z < j:
+								if w+3+h < y:
+									if weight_match(test_text_split[w+2+h]) and just_frag(test_text_split[w+3+h]):
+										weight = test_text_split[w+2+h]
+										comment = "fragment"
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight, comment)
+										z = z + 1
+										h = h + 1
+									elif weight_match(test_text_split[w+2+h]):
+										weight = test_text_split[w+2+h]
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight)
+										z = z + 1
+										h = h + 1
+									elif date_match1(test_text_split[w+2]) or date_match2(test_text_split[w+2]) or date_match3(test_text_split[w+2]):
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date)
+										z = z + 1
+										h = h + 1
+									else:
+										h = h + 1
+								elif w+2+h < y:
+									if weight_match(test_text_split[w+2+h]):
+										weight = test_text_split[w+2+h]
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight)
+										z = z + 1
+										h = h + 1
+									elif date_match1(test_text_split[w+2]) or date_match2(test_text_split[w+2]) or date_match3(test_text_split[w+2]):
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date)
+										z = z + 1
+										h = h + 1
+									else:
+										h = h + 1
 								else:
 									h = h + 1
-							elif w+2+h < y:
+									z = z + 1
+						if frag_multiple(test_text_split[w+1]):
+							j = int(filter(unicode.isdigit, test_text_split[w+1]))
+							z = 0
+							h = 0
+							while z < j:
+								if w+2+h < y:
+									if weight_match(test_text_split[w+2+h]):
+										weight = test_text_split[w+2+h]
+										comment = "fragment"
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight, comment)
+										z = z + 1
+										h = h + 1
+									else:
+										h = h + 1
+								else:
+									z = z + 1
+						if date_match1(test_text_split[w+1]) or date_match2(test_text_split[w+1]) or date_match3(test_text_split[w+1]):
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date)
+						if just_frag(test_text_split[w+1]):
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date)
+							
+					elif w + 1 < y:
+						if weight_match(test_text_split[w+1]):
+							weight = test_text_split[w+1]
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight)
+						if just_multiple(test_text_split[w+1]):
+							j = int(filter(unicode.isdigit, test_text_split[w+1]))
+							z = 0
+							h = 0
+							while z < j:
+								if w+3+h < y:
+									if weight_match(test_text_split[w+2+h]) and just_frag(test_text_split[w+3+h]):
+										weight = test_text_split[w+2+h]
+										comment = "fragment"
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight, comment)
+										z = z + 1
+										h = h + 1
+									elif weight_match(test_text_split[w+2+h]):
+										weight = test_text_split[w+2+h]
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight)
+										z = z + 1
+										h = h + 1
+									else:
+										h = h + 1
+								elif w+2+h < y:
+									if weight_match(test_text_split[w+2+h]):
+										weight = test_text_split[w+2+h]
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight)
+										z = z + 1
+										h = h + 1
+									else:
+										h = h + 1
+								else:
+									h = h + 1
+									z = z + 1
+						if frag_multiple(test_text_split[w+1]):
+							j = int(filter(str.isdigit, test_text_split[w+1]))
+							z = 0
+							h = 0
+							while z < j:
 								if weight_match(test_text_split[w+2+h]):
 									weight = test_text_split[w+2+h]
-									print(region, hoard_name, type, dynasty, leader, mint, date, weight)
+									comment = "fragment"
+									print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight, comment)
 									z = z + 1
 									h = h + 1
 								else:
 									h = h + 1
-							else:
-								h = h + 1
-								z = z + 1
-					if frag_multiple(test_text_split[w+1]):
-						j = int(filter(unicode.isdigit, test_text_split[w+1]))
-						z = 0
-						h = 0
-						while z < j:
-							if w+2+h < y:
+						if date_match1(test_text_split[w+1]) or date_match2(test_text_split[w+1]) or date_match3(test_text_split[w+1]):
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date)
+						if just_frag(test_text_split[w+1]):
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date, comment)
+	else:
+		if type_match(test_text):
+			type = test_text_split[1:(y-1)]
+		elif leader_match1(test_text) or leader_match2(test_text):
+			leader = test_text_split[0:(y-1)]
+		elif dynasty_match(test_text):
+			dynasty = test_text_split[1:(y-1)]
+			#also need to reset leader field with each new dynasty, as that dynasty may not have a leader type
+			leader = ""
+		elif hoard_name_match(test_text):
+			hoard_name = test_text_split[1:y]
+			weight = ""
+			tpq = ""
+		elif mint_line_match1(test_text) or mint_line_match2(test_text):
+			mint = ""
+			mint_line = test_text
+			y = len(test_text_split)
+			for w in range(y):
+				if mint_match1(test_text_split[w]) or mint_match2(test_text_split[w]):
+					if mint == "":
+						mint = test_text_split[w]
+					else:
+						mint = mint + " " + test_text_split[w]
+				if date_match1(test_text_split[w]) or date_match2(test_text_split[w]) or date_match3(test_text_split[w]):
+					date = test_text_split[w]
+					if w+2 < y:
+						if weight_match(test_text_split[w+1]) and just_frag(test_text_split[w+2]):
+							weight = test_text_split[w+1]
+							comment = "fragment"
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight, comment)
+						elif weight_match(test_text_split[w+1]):
+							weight = test_text_split[w+1]
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight)
+						if just_multiple(test_text_split[w+1]):
+							j = int(filter(unicode.isdigit, test_text_split[w+1]))
+							z = 0
+							h = 0
+							while z < j:
+								if w+3+h < y:
+									if weight_match(test_text_split[w+2+h]) and just_frag(test_text_split[w+3+h]):
+										weight = test_text_split[w+2+h]
+										comment = "fragment"
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight, comment)
+										z = z + 1
+										h = h + 1
+									elif weight_match(test_text_split[w+2+h]):
+										weight = test_text_split[w+2+h]
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight)
+										z = z + 1
+										h = h + 1
+									elif date_match1(test_text_split[w+2]) or date_match2(test_text_split[w+2]) or date_match3(test_text_split[w+2]):
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date)
+										z = z + 1
+										h = h + 1
+									else:
+										h = h + 1
+								elif w+2+h < y:
+									if weight_match(test_text_split[w+2+h]):
+										weight = test_text_split[w+2+h]
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight)
+										z = z + 1
+										h = h + 1
+									elif date_match1(test_text_split[w+2]) or date_match2(test_text_split[w+2]) or date_match3(test_text_split[w+2]):
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date)
+										z = z + 1
+										h = h + 1
+									else:
+										h = h + 1
+								else:
+									h = h + 1
+									z = z + 1
+						if frag_multiple(test_text_split[w+1]):
+							j = int(filter(unicode.isdigit, test_text_split[w+1]))
+							z = 0
+							h = 0
+							while z < j:
+								if w+2+h < y:
+									if weight_match(test_text_split[w+2+h]):
+										weight = test_text_split[w+2+h]
+										comment = "fragment"
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight, comment)
+										z = z + 1
+										h = h + 1
+									else:
+										h = h + 1
+								else:
+									z = z + 1
+						if date_match1(test_text_split[w+1]) or date_match2(test_text_split[w+1]) or date_match3(test_text_split[w+1]):
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date)
+						if just_frag(test_text_split[w+1]):
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date)
+							
+					elif w + 1 < y:
+						if weight_match(test_text_split[w+1]):
+							weight = test_text_split[w+1]
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight)
+						if just_multiple(test_text_split[w+1]):
+							j = int(filter(unicode.isdigit, test_text_split[w+1]))
+							z = 0
+							h = 0
+							while z < j:
+								if w+3+h < y:
+									if weight_match(test_text_split[w+2+h]) and just_frag(test_text_split[w+3+h]):
+										weight = test_text_split[w+2+h]
+										comment = "fragment"
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight, comment)
+										z = z + 1
+										h = h + 1
+									elif weight_match(test_text_split[w+2+h]):
+										weight = test_text_split[w+2+h]
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight)
+										z = z + 1
+										h = h + 1
+									else:
+										h = h + 1
+								elif w+2+h < y:
+									if weight_match(test_text_split[w+2+h]):
+										weight = test_text_split[w+2+h]
+										print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight)
+										z = z + 1
+										h = h + 1
+									else:
+										h = h + 1
+								else:
+									h = h + 1
+									z = z + 1
+						if frag_multiple(test_text_split[w+1]):
+							j = int(filter(str.isdigit, test_text_split[w+1]))
+							z = 0
+							h = 0
+							while z < j:
 								if weight_match(test_text_split[w+2+h]):
 									weight = test_text_split[w+2+h]
 									comment = "fragment"
-									print(region, hoard_name, type, dynasty, leader, mint, date, weight, comment)
+									print(region, hoard_name, tpq, type, dynasty, leader, mint, date, weight, comment)
 									z = z + 1
 									h = h + 1
 								else:
 									h = h + 1
-							else:
-								z = z + 1
-				elif w + 1 < y:
-					if weight_match(test_text_split[w+1]):
-						weight = test_text_split[w+1]
-						print(region, hoard_name, type, dynasty, leader, mint, date, weight)
-					if just_multiple(test_text_split[w+1]):
-						j = int(filter(unicode.isdigit, test_text_split[w+1]))
-						z = 0
-						h = 0
-						while z < j:
-							if w+3+h < y:
-								if weight_match(test_text_split[w+2+h]) and just_frag(test_text_split[w+3+h]):
-									weight = test_text_split[w+2+h]
-									comment = "fragment"
-									print(region, hoard_name, type, dynasty, leader, mint, date, weight, comment)
-									z = z + 1
-									h = h + 1
-								elif weight_match(test_text_split[w+2+h]):
-									weight = test_text_split[w+2+h]
-									print(region, hoard_name, type, dynasty, leader, mint, date, weight)
-									z = z + 1
-									h = h + 1
-								else:
-									h = h + 1
-							elif w+2+h < y:
-								if weight_match(test_text_split[w+2+h]):
-									weight = test_text_split[w+2+h]
-									print(region, hoard_name, type, dynasty, leader, mint, date, weight)
-									z = z + 1
-									h = h + 1
-								else:
-									h = h + 1
-							else:
-								h = h + 1
-								z = z + 1
-					if frag_multiple(test_text_split[w+1]):
-						j = int(filter(str.isdigit, test_text_split[w+1]))
-						z = 0
-						h = 0
-						while z < j:
-							if weight_match(test_text_split[w+2+h]):
-								weight = test_text_split[w+2+h]
-								comment = "fragment"
-								print(region, hoard_name, type, dynasty, leader, mint, date, weight, comment)
-								z = z + 1
-								h = h + 1
-							else:
-								h = h + 1
+						if date_match1(test_text_split[w+1]) or date_match2(test_text_split[w+1]) or date_match3(test_text_split[w+1]):
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date)
+						if just_frag(test_text_split[w+1]):
+							print(region, hoard_name, tpq, type, dynasty, leader, mint, date, comment)
